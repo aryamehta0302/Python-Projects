78d23c8cb0e32d1e572529c25c2de4c1d0ba7682
aryamehta0302 <148608606+aryamehta0302@users.noreply.github.com>
2024-09-09 22:21:09 +0530

    Add files via upload

---
+++ Weight Converter.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import *
+
+window=Tk()
+window.title("PythonGeeks")
+window.geometry("500x500")
+Label(window,text="WEIGHT CONVERTER",font=("Arial", 20 ),bg="black",fg='yellow').pack()
+
+kg=tk.IntVar()
+
+def convert_to_gram():
+    kg1=kg.get() 
+    gram = float(kg1)*1000
+    Label(window,text="This weight in grams would be",font=("Arial", 12 )).pack()
+    Label(window,text=gram,bg="red").pack()
+
+def convert_to_ounce():
+    kg1=kg.get()
+    ounce = float(kg1)*35.274
+    Label(window,text="This weight in ounce would be",font=("Arial", 12 )).pack()
+    Label(window,text=ounce,bg="red").pack()
+
+def convert_to_pound():
+    kg1=kg.get()
+    pound = float(kg1)*2.20462
+    Label(window,text="This weight in pound would be",font=("Arial", 12 )).pack()
+    Label(window,text=pound,bg="red").pack()
+
+Label(window,text="Enter the weight in Kgs",font=("Arial", 14 )).pack()
+Entry(window,textvariable=kg).pack()
+Button(window,text="Convert to Gram",bg="blue",command=convert_to_gram).pack()
+Button(window,text="Convert to Ounce",bg="blue",command=convert_to_ounce).pack()
+Button(window,text="Convert to Pound",bg="blue",command=convert_to_pound).pack()
+
+window.mainloop()
